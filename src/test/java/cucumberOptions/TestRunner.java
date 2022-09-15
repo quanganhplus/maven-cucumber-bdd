c1338ac9b40989e9dbaf230124585c7650af054d
Trinh Quang Anh <quanganh.plus@gmail.com>
2022-09-15 20:30:04 +0700

    update code

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -10,5 +10,4 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, plugin = { "pretty", "html:target/site/cucumber-report-default", "json:target/site/cucumber.json" }, snippets = SnippetType.CAMELCASE, tags = { "@displayed" })
 
 public class TestRunner {
-
 }
